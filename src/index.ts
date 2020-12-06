0b12c84f26273f4bf2df64ed0815e6dd0bc3c9db
Andre Brdoch <andrebrdoch@yahoo.de>
2020-12-06 15:32:13 +0100

    build script

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { route } from 'part:@sanity/base/router';
 import { BiBook } from 'react-icons/bi';
-import Tool from './components/Tool.tsx';
+import Tool from './components/Tool';
 
 const router = route('/', [route('/:typeName')]);
 
